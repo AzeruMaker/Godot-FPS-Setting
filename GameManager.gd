7c20ae40d57a1a0ffef33bbfc6798b9aa4057e96
AzeruMaker <azerumaker@live.fr>
2023-11-08 21:50:20 +0100

    First commit
    Add GameManager script (Part for FPS)

---
+++ GameManager.gd
@@ -0,0 +1,27 @@
+extends Node
+
+func set_fps(fps):
+	if typeof(fps) == TYPE_INT:
+		if vsynch_status() == DisplayServer.VSYNC_DISABLED :
+			Engine.max_fps = fps
+		else:
+			if fps == 0:
+				print ("Uncapped fps with VSync")
+				Engine.max_fps = fps
+			else:
+				print ("Warning : Cap fps with VSync")
+				Engine.max_fps = fps
+	else:
+		printerr ("Wrong call set fps with variable : " + fps)
+		
+func get_fps():
+	return (Engine.max_fps)
+
+func enable_vsync():
+	DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ADAPTIVE, 0)
+	
+func disable_vsync():
+	DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED, 0)
+
+func vsynch_status():
+	return (DisplayServer.window_get_vsync_mode(0))
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Godot FPS Setting
  
+Simple Godot GDScript to setup FPS and VSync for Godot 4.1
+
+Please read the documentation below to understand the script :
+Max Fps : https://docs.godotengine.org/en/stable/classes/class_engine.html#class-engine-property-max-fps
+VSync : https://docs.godotengine.org/en/stable/classes/class_displayserver.html#class-displayserver-method-window-set-vsync-mode 
+
+Keep in mind VSync should be disable in case of no limit FPS (Max FPS = 0)
+
+The list of VSync mode can be checked here (VSyncMode) :
+https://docs.godotengine.org/en/stable/classes/class_displayserver.html#enumerations
